6ba90e95ad53eebb379a2b14d8b082c14877918a
basarYargici <ibrahimbyargici@gmail.com>
2021-12-04 17:13:15 +0300

    Worked on network request

--- Volley/app/src/main/java/com/example/volley/MainActivity.kt
+++ Volley/app/src/main/java/com/example/volley/MainActivity.kt
@@ -17,18 +17,15 @@ import org.json.JSONObject
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
-    var user: JSONArray? = null
     val userList: MutableList<User> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        getData()
         binding.rwUser.layoutManager = LinearLayoutManager(this@MainActivity)
         binding.rwUser.adapter = UserAdapter(this@MainActivity, userList)
-
-
+        getData()
     }
 
     fun get() {
@@ -47,20 +44,16 @@ class MainActivity : AppCompatActivity() {
                     for (i in 0 until data.length()) {
                         Log.e("SPEC DATA", data.getJSONObject(i).get("email").toString())
                     }
-
-
                 } catch (e: JSONException) {
                     e.printStackTrace()
                 }
-
             },
             { it.printStackTrace() })
 
         Volley.newRequestQueue(this).add(req)
-
     }
 
-    fun getData(): JSONArray? {
+    private fun getData(): JSONArray? {
         val url = "https://reqres.in/api/users"
         var data: JSONArray? = null
 
@@ -71,26 +64,25 @@ class MainActivity : AppCompatActivity() {
                 try {
                     val jsonObject = JSONObject(res)
                     data = jsonObject.getJSONArray("data")
+                    data?.let {
+                        for (i in 0 until data!!.length()) {
+                            val id = data!!.getJSONObject(i)?.get("id").toString().toInt()
+                            val email = data!!.getJSONObject(i)?.get("email").toString()
+                            val firstName = data!!.getJSONObject(i)?.get("first_name").toString()
+                            val lastName = data!!.getJSONObject(i)?.get("last_name").toString()
+                            val avatar = data!!.getJSONObject(i)?.get("avatar").toString()
+
+                            Log.e("main", data!!.get(i).toString())
+                            userList.add(i, User(id, firstName, lastName, email, avatar))
+                            binding.rwUser.adapter = UserAdapter(this@MainActivity, userList)
+                        }
+                    }
                 } catch (e: JSONException) {
                     e.printStackTrace()
-                }
-                data?.let {
-                    for (i in 0 until data!!.length()) {
-                        val id = data!!.getJSONObject(i)?.get("id").toString().toInt()
-                        val email = data!!.getJSONObject(i)?.get("email").toString()
-                        val firstName = data!!.getJSONObject(i)?.get("first_name").toString()
-                        val lastName = data!!.getJSONObject(i)?.get("last_name").toString()
-                        val avatar = data!!.getJSONObject(i)?.get("avatar").toString()
-
-                        Log.e("main", data!!.get(i).toString())
-                        userList.add(i, User(id, firstName, lastName, email, avatar))
-                    }
                 }
             },
             { it.printStackTrace() })
         Volley.newRequestQueue(this@MainActivity).add(req)
-        binding.rwUser.adapter = UserAdapter(this@MainActivity, userList)
-
         return data
     }
 
