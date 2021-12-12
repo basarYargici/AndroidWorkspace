df3e03ebe6a43d53b5547ae673ba1fc6bccff5af
basarYargici <ibrahimbyargici@gmail.com>
2021-12-12 15:56:10 +0300

    databinding to pickup and summary fragments started

--- CupcakeApp/app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ CupcakeApp/app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -8,18 +8,20 @@ import java.util.*
 
 class OrderViewModel : ViewModel() {
 
-    private val _quantity = MutableLiveData<Int>(0)
+    private val _quantity = MutableLiveData<Int>()
+    private val _flavor = MutableLiveData<String>()
+    private val _date = MutableLiveData<String>()
+    private val _price = MutableLiveData<Double>()
     val quantity: LiveData<Int> = _quantity
-
-    private val _flavor = MutableLiveData<String>("")
     val flavor: LiveData<String> = _flavor
-
-    private val _date = MutableLiveData<String>("")
     val date: LiveData<String> = _date
-
-    private val _price = MutableLiveData<Double>(0.0)
     val price: LiveData<Double> = _price
 
+    val dateOptions: List<String> = getPickupOptions()
+
+    init {
+        resetOrder()
+    }
 
     fun setQuantity(numberCupcakes: Int) {
         _quantity.value = numberCupcakes
@@ -35,5 +37,25 @@ class OrderViewModel : ViewModel() {
 
     fun hasNoFlavorSet(): Boolean {
         return _flavor.value.isNullOrEmpty()
+    }
+
+    fun resetOrder() {
+        _quantity.value = 0
+        _flavor.value = ""
+        _date.value = dateOptions[0]
+        _price.value = 0.0
+    }
+
+    private fun getPickupOptions(): List<String> {
+        val options = mutableListOf<String>()
+        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+        val calendar = Calendar.getInstance()
+
+        // Create a list of dates starting with the current date and the following 3 dates
+        repeat(4) {
+            options.add(formatter.format(calendar.time))
+            calendar.add(Calendar.DATE, 1)
+        }
+        return options
     }
 }
