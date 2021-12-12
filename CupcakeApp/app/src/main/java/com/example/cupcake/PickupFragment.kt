090f6d889c86f8cfeb641f596a721dd053b2d3be
basarYargici <ibrahimbyargici@gmail.com>
2021-12-12 05:29:58 +0300

    Screens connected

--- CupcakeApp/app/src/main/java/com/example/cupcake/PickupFragment.kt
+++ CupcakeApp/app/src/main/java/com/example/cupcake/PickupFragment.kt
@@ -19,8 +19,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.cupcake.databinding.FragmentPickupBinding
 
 /**
@@ -54,7 +54,7 @@ class PickupFragment : Fragment() {
      * Navigate to the next screen to see the order summary.
      */
     fun goToNextScreen() {
-        Toast.makeText(activity, "Next", Toast.LENGTH_SHORT).show()
+        findNavController().navigate(R.id.action_pickupFragment_to_summaryFragment)
     }
 
     /**
