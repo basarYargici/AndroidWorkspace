335ff1ef7aaf46f113dd840bca9edf08d0f05ac5
basarYargici <ibrahimbyargici@gmail.com>
2021-12-13 04:57:31 +0300

    Dependencies added

--- GitProject/app/src/main/java/com/example/gitproject/MainActivity.kt
+++ GitProject/app/src/main/java/com/example/gitproject/MainActivity.kt
@@ -3,9 +3,4 @@ package com.example.gitproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
