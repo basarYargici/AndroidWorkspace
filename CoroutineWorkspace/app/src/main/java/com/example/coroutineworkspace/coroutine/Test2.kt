4271bc13b864a21f997a0e30d4cc9fafca564015
basarYargici <ibrahimbyargici@gmail.com>
2022-03-08 05:31:34 +0300

    Test2 added

---
+++ CoroutineWorkspace/app/src/main/java/com/example/coroutineworkspace/coroutine/Test2.kt
@@ -0,0 +1,27 @@
+package com.example.coroutineworkspace.coroutine
+
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+// Sequentially executes doWorld followed by "Done"
+fun main() = runBlocking {
+    doWorld()
+    doWorld() // runBlocking blocks the underlying thread, does not execute this line before 10 finished
+    println("Done")
+}
+
+// Concurrently executes both sections
+suspend fun doWorld() = coroutineScope { // this: CoroutineScope
+    launch {
+        delay(2000L)
+        println("World 2")
+    }
+    launch {
+        delay(1000L)
+        println("World 1")
+    }
+    println("Hello")
+}
+// coroutineScope line debug: 16 -> 20 -> 24 -> 17 ..
