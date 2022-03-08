226bf9a8cf2f8aa3bc1abef25991e8bf950ccdc3
basarYargici <ibrahimbyargici@gmail.com>
2022-03-08 05:14:35 +0300

    Test1 added

--- CoroutineWorkspace/app/src/main/java/com/example/coroutineworkspace/coroutine/Test1.kt
+++ CoroutineWorkspace/app/src/main/java/com/example/coroutineworkspace/coroutine/Test1.kt
@@ -8,6 +8,21 @@ fun main() = runBlocking { // this: CoroutineScope
     launch { // launch a new com.example.coroutineworkspace.coroutine and continue
         delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
         println("World!") // print after delay
+        delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
+        println("World!") // print after delay
+        delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
+        println("World!") // print after delay
     }
+
     println("Hello") // main com.example.coroutineworkspace.coroutine continues while a previous one is delayed
+    println("Hello1") // main com.example.coroutineworkspace.coroutine continues while a previous one is
+    // delayed
+
+    runBlocking {
+        println("Hello") // main com.example.coroutineworkspace.coroutine continues while a previous one is delayed
+        delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
+        println("World!") // print after delay
+        delay(8000L) // non-blocking delay for 1 second (default time unit is ms)
+        println("World!") // print after delay
+    }
 }
