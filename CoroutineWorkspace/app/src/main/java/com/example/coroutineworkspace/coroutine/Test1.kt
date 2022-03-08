6910b1dbab4018de7bf68cb57ac7d6ba18345088
basarYargici <ibrahimbyargici@gmail.com>
2022-03-08 04:56:27 +0300

    Repo initialized

---
+++ CoroutineWorkspace/app/src/main/java/com/example/coroutineworkspace/coroutine/Test1.kt
@@ -0,0 +1,13 @@
+package com.example.coroutineworkspace.coroutine
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking { // this: CoroutineScope
+    launch { // launch a new com.example.coroutineworkspace.coroutine and continue
+        delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
+        println("World!") // print after delay
+    }
+    println("Hello") // main com.example.coroutineworkspace.coroutine continues while a previous one is delayed
+}
