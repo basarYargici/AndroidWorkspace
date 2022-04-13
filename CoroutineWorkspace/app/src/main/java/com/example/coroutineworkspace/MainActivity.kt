9af2be4f20fad411a03353c05eca99993141ae35
basarYargici <ibrahimbyargici@gmail.com>
2022-04-13 16:26:04 +0300

    Async, job and sequential call examples added

--- CoroutineWorkspace/app/src/main/java/com/example/coroutineworkspace/MainActivity.kt
+++ CoroutineWorkspace/app/src/main/java/com/example/coroutineworkspace/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 const val TAG = "COROUTINETEST"
 
@@ -18,7 +19,96 @@ class MainActivity : AppCompatActivity() {
 
 //        exampleGlobalScope()
 //        exampleRunBlocking()
-        exampleJob()
+//        exampleJob()
+//        multipleRequestLong()
+//        multipleRequestBadApproach()
+//        multipleRequestGoodApproach()
+//        multipleRequestMakeAsyncLikeLaunch()
+    }
+
+
+    // Meaningless usage of async. You should wait till async is finished like making it sequential
+    private fun multipleRequestMakeAsyncLikeLaunch() {
+        GlobalScope.launch {
+            val time = measureTimeMillis {
+                val answer = async { doNetworkCall() }.await()
+                val answer1 = async { doNetworkCall2() }.await()
+                Log.d(TAG, "Answer is $answer")
+                Log.d(TAG, "Answer1 is $answer1")
+            }
+            Log.d(TAG, "Request took $time ms") // 4000 ms
+        }
+    }
+
+    /**
+     * there are basically two kind of execution
+    1. Sequential (Default)
+    2. Concurrent(achieved with async{} )
+    For instance if we have two function func1() & func2(), execution of func2() is dependent on func1() then
+    in that case we can use default version of coroutine and if they are independent of each other then we can
+    use concurrent with the help of async{}
+
+    with the sequential execution we run the code sequentially so if func1() takes 5sec for execution and
+    func2() takes 3 sec for execution then total elapsed time will be around 8 sec, however when this methods
+    are independent and their results are not required for execution of other fun then in that case execution
+    can be completed in 5 sec with the help of async(concurrent).
+
+    Async vs launch:
+    Async returns Deffered<Type> [Type:: calling function is returning]
+    launch returns Job.
+
+    await() -> methods are used to ensure the execution will go further until function is executed completely.
+    [e.g func1().await()]
+
+    We can also call async with LAZY pass below value in async constructor.
+    val one = async(start = CoroutineStart.LAZY) { func1()) }
+
+    one.start() // start the execution of func1
+
+     */
+    // Start coroutine ad return deferred instead of job, can be used to get result
+    private fun multipleRequestGoodApproach() {
+        GlobalScope.launch {
+            val time = measureTimeMillis {
+                val answer = async { doNetworkCall() } // last line will be returned
+                val answer1 = async { doNetworkCall2() } // last line will be returned
+                Log.d(TAG, "Answer is ${answer.await()}")
+                Log.d(TAG, "Answer1 is ${answer1.await()}")
+            }
+            Log.d(TAG, "Request took $time ms") // 2000s
+        }
+    }
+
+
+    // Start coroutine and return job
+    private fun multipleRequestBadApproach() {
+        GlobalScope.launch {
+            val time = measureTimeMillis {
+                var answer: String? = null
+                var answer1: String? = null
+                val job = launch { answer = doNetworkCall() }
+                val job1 = launch { answer1 = doNetworkCall2() }
+                job.join()
+                job1.join()
+                Log.d(TAG, "Answer is $answer")
+                Log.d(TAG, "Answer1 is $answer1")
+            }
+            Log.d(TAG, "Request took $time ms") // 2000s
+        }
+    }
+
+
+    private fun multipleRequestLong() {
+        GlobalScope.launch {
+            val time = measureTimeMillis {
+                val answer = doNetworkCall() // go and finish function, then assign it to variable
+                val answer1 = doNetworkCall2()
+                Log.d(TAG, "Answer is $answer")
+                Log.d(TAG, "Answer1 is $answer1")
+            }
+
+            Log.d(TAG, "Request took $time ms") // 4000s
+        }
     }
 
     private fun exampleJob() {
@@ -66,6 +156,13 @@ class MainActivity : AppCompatActivity() {
 
     suspend fun doNetworkCall(): String {
         delay(2000)
-        return "Network Call Demo"
+        val text = "Network Call Demo"
+        return text
+    }
+
+    suspend fun doNetworkCall2(): String {
+        delay(2000)
+        val text = "Network Call2 Demo"
+        return text
     }
 }
