cb1fb652c94ec17354ea29bb32df590383ee1bf6
basarYargici <ibrahimbyargici@gmail.com>
2022-05-04 03:59:51 +0300

    hw tests are written

--- TestingWorkspace/app/src/main/java/com/example/testingworkspace/Workspace.kt
+++ TestingWorkspace/app/src/main/java/com/example/testingworkspace/Workspace.kt
@@ -2,4 +2,33 @@ package com.example.testingworkspace
 
 class Workspace {
     fun sumTwoIntegers(numberA: Int, numberB: Int) = numberA + numberB
+
+    /**
+     * Returns n-th fibonacci number
+     * fib(0) = 0
+     * fib(1) = 1
+     * fib(n) = fib(n-2) + fib(n-1)
+     */
+    fun fib(n: Int): Long {
+        if (n == 0 || n == 1) {
+            return n.toLong()
+        }
+        var a = 0L
+        var b = 1L
+        var c = 1L
+        (1 until n).forEach { i ->
+            c = a + b
+            a = b
+            b = c
+        }
+        return c
+    }
+
+    /**
+     * checks brace correctness
+     * e.g. "(a*b))" should return false
+     */
+    fun checkBrace(string: String): Boolean {
+        return string.count { it == '(' } == string.count { it == ')' }
+    }
 }
--- TestingWorkspace/app/src/test/java/com/example/testingworkspace/WorkspaceTest.kt
+++ TestingWorkspace/app/src/test/java/com/example/testingworkspace/WorkspaceTest.kt
@@ -16,4 +16,82 @@ class WorkspaceTest : TestCase() {
         // then
         assertEquals(expected, actual)
     }
+
+    @Test
+    fun testNumberZeroShouldReturnZero() {
+        // given
+        val actual = Workspace().fib(0)
+
+        // when
+        val expected = 0L
+
+        // then
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testNumberOneShouldReturnOne() {
+        // given
+        val actual = Workspace().fib(1)
+
+        // when
+        val expected = 1L
+
+        // then
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testNumberThreeShouldReturnTwo() {
+        // given
+        val actual = Workspace().fib(3)
+
+        // when
+        val expected = 2L
+
+        // then
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testNumberFiveShouldReturnFive() {
+        // given
+        val actual = Workspace().fib(5)
+
+        // when
+        val expected = 5L
+
+        // then
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testOneOpeningOneClosingParenthesisShouldReturnTrue() {
+        // given
+        val actual = Workspace().checkBrace("(a*b)")
+
+        // when
+        // then
+        assertTrue(actual)
+    }
+
+    @Test
+    fun testOneOpeningTwoClosingParenthesisShouldReturnFalse() {
+        // given
+        val actual = Workspace().checkBrace("(a*b))")
+
+        // when
+        // then
+        assertFalse(actual)
+    }
+
+    @Test
+    fun testZeroOpeningZeroClosingParenthesisShouldReturnTrue() {
+        // given
+        val actual = Workspace().checkBrace("a*b")
+
+        // when
+        // then
+        assertTrue(actual)
+    }
 }
